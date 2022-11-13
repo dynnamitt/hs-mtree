196e75c2007422ee8ceefda33bbc88d10e568902
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-11-13 22:46:38 +0100

    cpu-oven part 1

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Haskell >= mtree
 
+## GHC Version
+
+GHC version must be aligned with the .cabal file's 
+
+-  build-depends: base >= x.x.x 
+
+Use `ghcup tui` -> i(install) -> s(et)
+
 ```mermaid
 stateDiagram-v2
     s1 : mtree $PATH
--- app/Main.hs
+++ app/Main.hs
@@ -1,52 +1,23 @@
+module Main where
+
+import Data.Maybe
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import System.Directory.Contents
+import System.Environment (getArgs, getProgName)
 import System.Exit
-import System.Environment
-import System.Directory
-import Control.Exception
-import System.IO.Error
-import Data.Typeable
-import GHC.IO.Exception
 
-
+toolDescription :: String
 toolDescription = "Traverse directory tool"
 
 main :: IO ()
-main = do 
+main = do
   startPath <- parseArgs
-  putStrLn $ "running down:" ++ startPath
-  getFiles startPath
-
-
-getFiles :: FilePath -> IO ()
-getFiles startDir = do
-  files <- tryJust handleListErrors (listDirectory startDir)
-  case files of
-    Left errMsg -> putStrLn $ show errMsg
-    Right fileNames -> mapM_ hFile fileNames
-  where
-    hFile f = putStrLn f
-
-data ListError = DoesNotExist
-                | NoAccess
-                | DiskFull
-                | HardwareIssue
-                | BadName
-                | WrongType
-                | OtherIOErr IOError
-                deriving (Show,Eq)
-
-handleListErrors :: IOError -> Maybe ListError
-handleListErrors err
-  -- https://hackage.haskell.org/package/directory-1.3.8.0/docs/System-Directory.html#v:listDirectory
-  -- can fail in 6 expected ways
-  | isDoesNotExistError err = Just DoesNotExist
-  | isPermissionError err = Just NoAccess
-  | isFullError err = Just DiskFull 
-  | otherwise = -- lower-level 
-      case err of
-        IOError _ HardwareFault _ _ _ _ -> Just HardwareIssue
-        IOError _ InvalidArgument _ _ _ _ -> Just BadName
-        IOError _ InappropriateType _ _ _ _ -> Just WrongType
-        otherwise -> Just $ OtherIOErr err
+  putStrLn startPath
+  t0 <- buildDirTree startPath
+  let t2 = walkDirTree "./code" $ fromJust t0
+  let t = fromJust t2
+  T.putStrLn $ drawDirTree t
 
 -- help
 usage :: IO ()
@@ -54,7 +25,8 @@ usage = do
   prog <- getProgName
   putStrLn toolDescription
   putStrLn "usage:"
-  putStrLn $ "  " ++ prog ++ " <Directory>" 
+  putStrLn $ "  " ++ prog ++ " <Directory>"
+
 --
 -- args or death
 parseArgs :: IO String
@@ -65,8 +37,6 @@ parseArgs = do
     then do
       usage
       exitWith $ ExitFailure 1
-    else do 
+    else do
       -- TODO errorhandling
       return $ head args
-
-
