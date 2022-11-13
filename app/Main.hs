53d2b681c4dfe9e97e198b6eb75f7b1cefff17f3
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-11-13 15:22:23 +0100

    first dir

--- app/Main.hs
+++ app/Main.hs
@@ -1,2 +1,36 @@
+import System.Exit
+import System.Environment
+import System.Directory
+
+toolDescription = "Traverse directory tool"
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do 
+  startPath <- parseArgs
+  putStrLn $ "Hello, World!" ++ startPath 
+  files <- listDirectory startPath
+  mapM_ putStrLn files
+
+
+-- help
+usage :: IO ()
+usage = do
+  prog <- getProgName
+  putStrLn toolDescription
+  putStrLn "usage:"
+  putStrLn $ "  " ++ prog ++ " <Directory>" 
+--
+-- args or death
+parseArgs :: IO String
+parseArgs = do
+  let argsLen = 1
+  args <- getArgs -- IO
+  if length args /= argsLen
+    then do
+      usage
+      exitWith $ ExitFailure 1
+    else do 
+      -- TODO errorhandling
+      return $ head args
+
+
