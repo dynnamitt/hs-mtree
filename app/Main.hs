0f64c04bdb8bae4ff4b484ce6403024265d964f7
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-11-13 16:53:25 +0100

    err hnd

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,52 @@
 import System.Exit
 import System.Environment
 import System.Directory
+import Control.Exception
+import System.IO.Error
+import Data.Typeable
+import GHC.IO.Exception
+
 
 toolDescription = "Traverse directory tool"
 
 main :: IO ()
 main = do 
   startPath <- parseArgs
-  putStrLn $ "Hello, World!" ++ startPath 
-  files <- listDirectory startPath
-  mapM_ putStrLn files
+  putStrLn $ "running down:" ++ startPath
+  getFiles startPath
 
+
+getFiles :: FilePath -> IO ()
+getFiles startDir = do
+  files <- tryJust handleListErrors (listDirectory startDir)
+  case files of
+    Left errMsg -> putStrLn $ show errMsg
+    Right fileNames -> mapM_ hFile fileNames
+  where
+    hFile f = putStrLn f
+
+data ListError = DoesNotExist
+                | NoAccess
+                | DiskFull
+                | HardwareIssue
+                | BadName
+                | WrongType
+                | OtherIOErr IOError
+                deriving (Show,Eq)
+
+handleListErrors :: IOError -> Maybe ListError
+handleListErrors err
+  -- https://hackage.haskell.org/package/directory-1.3.8.0/docs/System-Directory.html#v:listDirectory
+  -- can fail in 6 expected ways
+  | isDoesNotExistError err = Just DoesNotExist
+  | isPermissionError err = Just NoAccess
+  | isFullError err = Just DiskFull 
+  | otherwise = -- lower-level 
+      case err of
+        IOError _ HardwareFault _ _ _ _ -> Just HardwareIssue
+        IOError _ InvalidArgument _ _ _ _ -> Just BadName
+        IOError _ InappropriateType _ _ _ _ -> Just WrongType
+        otherwise -> Just $ OtherIOErr err
 
 -- help
 usage :: IO ()
